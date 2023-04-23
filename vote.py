edbb74b0c095fbe6ac74d60ef1144eb65fc82cd0
DeYiC <vc441@scarletmail.rutgers.edu>
2023-04-23 00:30:07 -0400

    Removed functionality from vote

--- vote.py
+++ vote.py
@@ -1,24 +1,4 @@
-import sqlite3
-from database import *
 from blockchain import *
-
-# Check vote is completed or not
-def isVoteCompleted(vote_id):
-    conn = get_db_connection()
-    vote = conn.execute('SELECT * FROM votes WHERE vote_id = ?', (vote_id)).fetchone()
-    vote_dict = row_to_dict(vote)
-
-    if vote_dict['vote_isComplete']:
-        conn.close()
-        return True
-
-    voted = vote_dict['op1_ct'] + vote_dict['op2_ct'] + 1
-    if voted >= vote_dict['vote_participants_ct']:
-        conn.execute('UPDATE votes SET vote_isComplete = TRUE WHERE vote_id = ?', (vote_id,))
-        conn.commit()
-        conn.close()
- 
-    return False
 
 # Check duplicate vote in blockchain
 def isDuplicateVote(blockchain, pk, hs):
